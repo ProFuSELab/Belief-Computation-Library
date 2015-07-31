cc29cdab6865178900d7fffde194e79a31f14deb
Lalintha Polpitiya <Lalintha@users.noreply.github.com>
2015-07-30 20:24:42 -0400

    Improved the desing, added the dsvector.h header file

---
+++ data_structures/main.cpp
@@ -0,0 +1,47 @@
+#include "vector/dsvector.h"
+
+using namespace std;
+
+int main()
+{
+        int index;
+        double element;
+        DSVector ds_vector;
+        ds_vector.debugOff();
+        ds_vector.getSingletons();
+        index = ds_vector.calFocalIndexAscending();
+        element = ds_vector.accessFocalElement(index);
+        ds_vector.genIncreasingMassValues();
+        ds_vector.fillingBeliefVecAscending();
+        ds_vector.calBelief();
+        ds_vector.fillingBeliefInvVecAscending();
+        ds_vector.calPlausibility();
+        ds_vector.printSingletonVector();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.printSingletonVector();
+        ds_vector.printFocalElements();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.addFocalEleSingleton();
+        ds_vector.printSingletonVector();
+        ds_vector.printFocalElements();
+            
+        return 0;
+}
