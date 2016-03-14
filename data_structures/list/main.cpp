79b5a7de9cb8bf2e125600b0019fa9d8cdb56aa0
lalintha <lalintha@gmail.com>
2016-03-14 08:52:06 -0400

    Access time experiments with DS-List for 10000 iterations

---
+++ data_structures/list/main.cpp
@@ -0,0 +1,35 @@
+#include "dslist.h"
+
+using namespace std;
+
+int main()
+{
+	double experiment_time, total_time = 0;
+	vector <int> param;
+	DSList ds_list;
+	ds_list.debugOff();
+	ds_list.readSingletons();
+
+	for (int fod = 1; fod <= 20; fod++)
+	{       
+		for (int rounds = 0; rounds < 10000; rounds++)
+		{       
+			ds_list.genRandomList(fod);
+			for (int i = 0; i < fod; i++)
+			{       
+				if (rand() % 3 != 0)
+				param.push_back(i);
+			}
+			experiment_time = ds_list.accessFocalElementIndexVec(param);
+			param.clear();
+			total_time += experiment_time;
+		}
+		cout << "FoD size : " << fod << " Time spent : " << total_time << endl;
+		total_time = 0;
+	}
+	
+	return 0;
+}
+
+
+
